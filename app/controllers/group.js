8d6f44b12478c675b3132074c7babcdb1a775284
bergero8 <academichw@gmail.edu>
2014-04-28 22:10:10 -0500

    With user query added

--- app/controllers/group.js
+++ app/controllers/group.js
@@ -4,7 +4,7 @@
 	 * @uses core
 	 */
 	var APP = require("core");
-	var Cloud = require('ti.cloud');
+	var Cloud = require("ti.cloud");
 
 	/**
 	 * Initializes the controller
@@ -26,36 +26,45 @@
 	};
 
 	$.btnFriend.addEventListener('click', function() {
-		Cloud.Friends.add({
-			//user_ids: checked.join(",")
-			user_ids: 
+		Cloud.Users.query({
+			page: 1,
+			per_page: 10,
+			where: {
+				//username: 'testfriend2'
+				first_name: $.txtFindFriend.value
+
+			}
 		}, function(e) {
 			if(e.success) {
-				alert('Friend(s) added');
+				var rows = [];
+
+				for(var i = 0; i < e.users.length; i++) {
+					var user = e.users[i];
+					var row = Ti.UI.createTableViewRow({
+						layout: "vertical",
+						hasChild: true,
+						name: user.username,
+
+					});
+					var title = Ti.UI.createLabel({
+						text: user.username,
+						font: {
+							fontFamily: "bold",
+							fontSize: "20dp"
+						},
+						left: "10dp"
+					});
+
+					row.add(title);
+					rows.push(row);
+				}
+
+				$.userTable.setData(rows);
 			} else {
 				alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
 			}
 		});
 	});
 
-	$.btnFindFriend.addEventListener('click', function() {
-		Cloud.Friends.search({
-			//user_id: searchID
-			user_id: $.txtFriend.value
-		}, function(e) {
-			if(e.success) {
-				alert('Success:\n' +
-					'Count: ' + e.users.length);
-				for(var i = 0; i < e.users.length; i++) {
-					var user = e.users[i];
-					alert('id: ' + user.id + '\n' +
-						'first name: ' + user.first_name + '\n' +
-						'last name: ' + user.last_name);
-				}
-			} else {
-				alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
-			}
-		});
-	});
 	// Kick off the init
 	$.init();
